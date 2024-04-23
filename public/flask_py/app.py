6839fbd854a9a50cc27c759be4359023c6a76134
Michelle Wu <mishawu@berkeley.edu>
2024-04-23 16:13:23 -0700

    flask

--- public/flask_py/app.py
+++ public/flask_py/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import render_template
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('hello.html', name="df")
+    return render_template('aitranslator.html')
+
+if __name__ == '__main__':
+    
+    app.debug = True
+    app.run()
