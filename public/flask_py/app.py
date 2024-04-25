95c62ed74e016409a0995544d34dd643bb872415
Apratim08 <apratimbanerjee@berkeley.edu>
2024-04-24 21:01:22 -0700

    base.html done

--- public/flask_py/app.py
+++ public/flask_py/app.py
@@ -1,55 +1,15 @@
+from flask import Flask, render_template
 
-from flask import Flask, flash, request, redirect, render_template, url_for
-from image_util import allowed_file, parse_image
-
-app = Flask(__name__)
-
-# 
-# ========
-# ENDPOINTS
-# ========
-# 
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+def base():
+    return render_template('base.html')
 
-#TODO
-# THIS IS A DUMMY FOR ADDING VARIABLES TO A TEMPLATE
-@app.route("/translator")
-def dummy():
-    return render_template('hello.html', name="this is the name")
+@app.route("/word_bank")
+def word_bank():
+    return render_template('wordbank.html')
 
-#TODO implement template and change
-# referencing flask documentation for file uploads
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # check if the post request has the file part
-    if 'image' not in request.files:
-        flash('No file part')
-        return redirect(url_for('/'))
-    file = request.files['image']
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(url_for('/'))
-    if file and allowed_file(file.filename):
-        image = request.files['image']  
-        
-        # get populated dictionary
-        image_info = parse_image(image)
-
-        #TODO use real template link here
-        return render_template('FIXME.html', image_info=image_info)    
-    else:
-        flash('Invalid File Type')
-        return redirect(url_for('/'))
-
-#TODO implement word list visualization
-@app.route("/wordlist")
-def word_list():
-    return render_template('FIXME.html', words=[])
 
 if __name__ == '__main__':
-    
-    app.debug = True
-    app.run()
+    app.run(debug=True)
