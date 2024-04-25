a8285cff4a2644a09fdd6a78c684d18b2cb81af8
Apratim08 <apratimbanerjee@berkeley.edu>
2024-04-25 13:41:03 -0700

    aitranslator page done

--- public/flask_py/app.py
+++ public/flask_py/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_folder='static')
+
+# Configure a directory to save uploaded files
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure the upload folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
 def base():
@@ -14,10 +23,29 @@ def word_bank():
 def individualword():
     return render_template('individualword.html')
 
-@app.route("/aitranslator")
-def aitranslator():
-    return render_template('aitranslator.html')
+@app.route("/aitranslator", defaults={'filename': None})
+@app.route("/aitranslator/<filename>")
+def aitranslator(filename):
+    if filename:
+        image_url = url_for('static', filename=f'uploads/{filename}')
+    else:
+        image_url = None
+    return render_template('aitranslator.html', filename=image_url)
 
+@app.route("/upload", methods=['POST'])
+def upload_file():
+    if 'fileInput' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['fileInput']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return redirect(url_for('aitranslator', filename=filename))
 
 if __name__ == '__main__':
     app.run(debug=True)
